12a06d45fc96191d061b483491263b1bb5fe23d4
Alexandros Collie <amcollie@gmail.com>
2020-06-04 04:48:11 -0400

    Add user and message table. Add messages from table to chat form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'alexandros', password: 'password123')
+User.create(username: 'evgeny', password: 'password123')
+User.create(username: 'jonshow', password: 'password123')
+User.create(username: 'arya', password: 'password123')
+User.create(username: 'frodo', password: 'password123')
+User.create(username: 'gandalf', password: 'password123')
